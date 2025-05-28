4ce941937dd4ae0083d69001c00e2e89e492404e
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2025-05-28 18:08:13 +0530

    added zod validation from a self published npm package

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "../generated/prisma/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 import { verify } from "hono/jwt";
-
+import { createBlog, updateBlog } from "@abhilashdash/medium-common";
 export const blog = new Hono<{
   Bindings: {
     DATABASE_URL: string;
@@ -15,7 +15,7 @@ export const blog = new Hono<{
 
 //used to protect the blog routes. Only the authorized users can see the blogs
 blog.use("/*", async (c, next) => {
-  //take the token from the usr from header and return the authorId
+  //take the token from the user from header and return the authorId
   const token = c.req.header("authorization") || "";
   console.log("token: ",token);
   const decodedToken = await verify(token, c.env.JWT_SECRET);
@@ -37,20 +37,24 @@ blog.post("/", async (c) => {
   const prisma = new PrismaClient( {datasourceUrl: c.env.DATABASE_URL }).$extends(withAccelerate());
   try {
     const body = await c.req.json();
-    const userId = c.get("authorId");
-    console.log("userId as number", Number(userId));
-    console.log("userId as parseInt", parseInt(userId));
-
-    const blogByUser = await prisma.blog.create({
-      data: {
-        title: body.title,
-        content: body.content,
-        authorId: Number(userId),
-      },
-    });
-
-    //taking the id from body???
-    return c.json({ message: "Blog created", id: blogByUser.id });
+    const {success} = createBlog.safeParse(body);
+    if(success) {
+      const userId = c.get("authorId");
+      console.log("userId as number", Number(userId));
+      console.log("userId as parseInt", parseInt(userId));
+  
+      const blogByUser = await prisma.blog.create({
+        data: {
+          title: body.title,
+          content: body.content,
+          authorId: Number(userId),
+        },
+      });
+        return c.json({ message: "Blog created", id: blogByUser.id });
+    } else {
+    c.status(411);
+    return c.json({ msg: "Give correct data types"});
+    }
   } catch (error) {
     c.status(411);
     return c.json({ msg: "Blog didn't get created", error: error });
@@ -63,6 +67,8 @@ blog.put("/", async (c) => {
 
   //take the data to be updated from body
   const body = await c.req.json();
+  const {success} = updateBlog.safeParse(body);
+  if(!success) { c.status(413); return c.json({msg:"Give correct data types"})};
   try {
     const updatedBlog = await prisma.blog.update({
       where: {
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,68 +1,83 @@
-import { PrismaClient } from '../generated/prisma/edge';
-import { withAccelerate } from '@prisma/extension-accelerate'
-import { Hono } from 'hono'
-import { jwt, decode, sign, verify } from 'hono/jwt'
+import { PrismaClient } from "../generated/prisma/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { Hono } from "hono";
+import { jwt, decode, sign, verify } from "hono/jwt";
+import { signInSchema, signUpSchema } from "@abhilashdash/medium-common";
 
 export const user = new Hono<{
   Bindings: {
-    DATABASE_URL: string,
-    JWT_SECRET: string
-  }
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
 }>();
 
-user.post('/signup', async (c) => {
-  const prisma = new PrismaClient({ datasourceUrl: c.env.DATABASE_URL }).$extends(withAccelerate())
+user.post("/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
   try {
-    //take data from FE to BE 
+    //take data from FE to BE
     const body = await c.req.json();
-
+    const { success } = signUpSchema.safeParse(body);
     //take the data from the frontend and save it in the database
-    const user = await prisma.user.create({
-      data: {
-        name: body.name,
-        email: body.email,
-        password: body.password
-      }
-    })
+    if (success) {
+      const user = await prisma.user.create({
+        data: {
+          name: body.name,
+          email: body.email,
+          password: body.password,
+        },
+      });
 
-    //after adding the user in DDB, use it to return a token to user
-    const payload = {
-      userId: user.id,
-      email: user.email,
-      name: user.name
+      //after adding the user in DDB, use it to return a token to user
+      const payload = {
+        userId: user.id,
+        name: user.name,
+      };
+      const secret = c.env.JWT_SECRET;
+      const token = await sign(payload, secret);
+
+      // return c.json({ jwt: token });
+      return c.json({ message: "SignUP successful", jwt: token });
+    } else {
+      c.status(413);
+      return c.json({ message: "Give correct data types" });
     }
-    const secret = c.env.JWT_SECRET
-    const token = await sign(payload, secret)
-
-    // return c.json({ jwt: token });
-    return c.json({message: "SignUP successful", jwt: token })
   } catch (error) {
     console.log("Error: ", error);
-    return c.json({ message: "Kuch toh gadbad hai!", error: error })
+    return c.json({ message: "Kuch toh gadbad hai!", error: error });
   }
-})
+});
 
 user.post("/signin", async (c) => {
-  const prisma = new PrismaClient({ datasourceUrl: c.env.DATABASE_URL }).$extends(withAccelerate());
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
   const body = await c.req.json();
+  const { success } = signInSchema.safeParse(body);
   try {
-    const user = await prisma.user.findUnique({
-      where: {
-        email: body.email,
-        password: body.password
+      if(success) {
+      const user = await prisma.user.findUnique({
+        where: {
+          email: body.email,
+          password: body.password,
+        },
+      });
+      if (!user) {
+        c.status(404);
+        return c.json({ message: "You need to singup first!" });
       }
-    })
-    if (!user) {
-      c.status(404);
-      return c.json({ message: "You need to singup first!" })
-    }
-    const payload = { userId: user.id }
-    const token = await sign(payload, c.env.JWT_SECRET);
+      const payload = { userId: user.id };
+      const token = await sign(payload, c.env.JWT_SECRET);
   
-    return c.json({ jwt: token });
+      return c.json({ jwt: token });
+  } else {
+    c.status(411);
+    return c.json({ message: "Give correct data types" });
+  }
   } catch (error) {
     c.status(411);
     console.log("Error: ", error);
-    return c.json({message: "Error in singning in"})
+    return c.json({ message: "Error in singning in" });
   }
-})
+});
